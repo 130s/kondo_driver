3537988aebc75c32b5bedcf5d2246a7b01a147a6
Ron <ryosuke.tajima@gmail.com>
2015-03-19 19:05:37 +1000

    changing structure. no luck yet.

--- src/kondo_ics_driver.cpp
+++ src/kondo_ics_driver.cpp
@@ -32,6 +32,49 @@ int radian_to_pulse (double radian)
     return CNT_PULSE + radian/RADIAN_PER_PULSE;
 }
 
+class KondoMotor {
+private:
+    bool motor_power;
+    ros::ServiceServer power_service;
+    int id;
+public:
+    double cmd, pos, vel, eff;
+    std::string joint_name;
+    bool set_power (kondo_driver::setPower::Request &req, kondo_driver::setPower::Response &res) {
+	ROS_INFO("id %d, request: %d", this->id, req.request);
+	motor_power = req.request;
+	res.result = req.request;
+	return true;
+    }
+    KondoMotor (ros::NodeHandle nh, std::string actuator_name, hardware_interface::JointStateInterface& state_interface, hardware_interface::PositionJointInterface& pos_interface) {
+	cmd=0, pos=0;
+	if (nh.getParam(std::string("/kondo_driver/")+actuator_name+std::string("/id"), id)) {
+	    ROS_INFO("id: %d", id);
+	}
+	if (nh.getParam(std::string("/kondo_driver/")+actuator_name+std::string("/joint"), joint_name)) {
+	    ROS_INFO("joint: %s", joint_name.c_str());
+	}
+	hardware_interface::JointStateHandle state_handle(joint_name, &pos, &vel, &eff);
+	state_interface.registerHandle(state_handle);
+	hardware_interface::JointHandle pos_handle(state_interface.getHandle(joint_name), &cmd);
+	pos_interface.registerHandle(pos_handle);
+	power_service = nh.advertiseService(actuator_name+std::string("/set_power"), &KondoMotor::set_power, this);
+    }
+    void update (void) {
+#if 0
+	int pulse_cmd = 0;
+	if (motor_power == true) {
+	    pulse_cmd = radian_to_pulse(cmd);
+	}
+	//int pulse_ret = ics_pos(&ics, i, pulse_cmd);
+	int pulse_ret = 0;
+	//pos = pulse_to_radian (pulse_ret);
+	//pos = cmd;
+	//ROS_INFO("pulse[%d]:cmd, ret: %d, %d\n", id, pulse_cmd, pulse_ret);
+#endif
+	ROS_INFO("pulse[%d]:cmd, pos: %f, %f\n", id, cmd, pos);
+    }
+};
 
 class KondoICSHW : public hardware_interface::RobotHW
 {
@@ -39,32 +82,54 @@ class KondoICSHW : public hardware_interface::RobotHW
     ICSData ics; // ICS hardware resource
     hardware_interface::JointStateInterface jnt_state_interface;
     hardware_interface::PositionJointInterface jnt_pos_interface;
-    double cmd[MAX_MOTOR_NUM];
-    double pos[MAX_MOTOR_NUM];
-    double vel[MAX_MOTOR_NUM];
-    double eff[MAX_MOTOR_NUM];
-    std::vector<std::string> joint_name;
-    std::vector<std::string> motor_name;
-    static bool motor_power;
+    std::vector<KondoMotor> actuator_vector;
   public:
-    static bool set_power (kondo_driver::setPower::Request &req, kondo_driver::setPower::Response &res) {
-	ROS_INFO("request: %d", req.request);
-	motor_power = req.request;
-	res.result = req.request;
-	return true;
-    }
-    KondoICSHW () : joint_name(MAX_MOTOR_NUM, "none"), motor_name(MAX_MOTOR_NUM, "none") { 
-	motor_power = false;
-	pos[0] = pos[1] = 0;
-	vel[0] = vel[1] = 0;
-
+    KondoICSHW (ros::NodeHandle nh, int num, char** actuators) {
 	if (ics_init(&ics) < 0) {
 	    ROS_ERROR ("Could not init: %s\n", ics.error);
-	    exit (0);
+	    // exit (0);
 	}
-	listMotors();
+	//listMotors();
+	// Load parameters
+	for (int i=0; i<num; i++) {
+	    std::string actuator_name = std::string("/kondo_driver/")+std::string(actuators[i]);
+	    if (!nh.hasParam(actuator_name)) {
+		ROS_WARN("No parameter for actuator %s", actuator_name.c_str());
+	    }
+	    KondoMotor* actuator = new KondoMotor(nh, std::string(actuators[i]), jnt_state_interface, jnt_pos_interface);
+	    actuator_vector.push_back(*actuator);
+
+	    ROS_INFO("joint_name2: %s", actuator->joint_name.c_str());
+	    hardware_interface::JointStateHandle state_handle(actuator->joint_name, &actuator->pos, &actuator->vel, &actuator->eff);
+	    jnt_state_interface.registerHandle(state_handle);
+	}
+	registerInterface(&jnt_state_interface);
+	for (int i=0; i<num; i++) {
+	    std::string actuator_name = std::string("/kondo_driver/")+std::string(actuators[i]);
+	    if (!nh.hasParam(actuator_name)) {
+		ROS_WARN("No parameter for actuator %s", actuator_name.c_str());
+	    }
+	    KondoMotor* actuator = new KondoMotor(nh, std::string(actuators[i]), jnt_state_interface, jnt_pos_interface);
+	    actuator_vector.push_back(*actuator);
+
+	    ROS_INFO("joint_name2: %s", actuator->joint_name.c_str());
+	    hardware_interface::JointHandle pos_handle(jnt_state_interface.getHandle(actuator->joint_name), &actuator->cmd);
+	    jnt_pos_interface.registerHandle(pos_handle);
+	}
+	registerInterface(&jnt_pos_interface);
+#if 0
+	// Setup actuator
+	std::string driver_name = std::string("/kondo_driver/")+actuator_name;
+	ROS_INFO("actuator: %s", actuator_name.c_str());
+	int id;
+	if (nh.getParam(driver_name+sftd::string("/id"), id)) {
+	    ROS_INFO("id: %d", id);
+	}
+	std::string joint_name;
+	if (nh.getParam(driver_name+std::string("/joint"), joint_name)) {
+	    ROS_INFO("joint: %s", joint_name.c_str());
+	}
 	// Read parameters
-	ros::NodeHandle nh;
 	for (int i=0; i<MAX_MOTOR_NUM; i++) {
 	    std::string motor_str, joint_str;
 	    motor_str = motor_name[i];
@@ -92,37 +157,21 @@ class KondoICSHW : public hardware_interface::RobotHW
 	    }
 	}
 	registerInterface(&jnt_pos_interface);
-
-#if 0
-	// Start set_power service
-	for (int i=0; i<MAX_MOTOR_NUM; i++) {
-	    if (joint_name[i] != "none") {
-		ros::ServiceServer service = nh.advertiseService(joint_name[i]+std::string("/set_power"), set_power);
-	    }
-	}
 #endif
     }
     void update (void) {
-	for (int i=0; i<MAX_MOTOR_NUM; i++) {
-	    if (motor_name[i] != "none") {
-		int pulse_cmd = 0;
-		if ( motor_power == true) {
-		    pulse_cmd = radian_to_pulse(cmd[i]);
-		}
-		int pulse_ret = ics_pos(&ics, i, pulse_cmd);
-		pos[i] = pulse_to_radian (pulse_ret);
-		// ROS_INFO("pulse[%d]:cmd, ret: %d, %d\n", i, pulse_cmd, pulse_ret);
-	    }
+	for (int i=0; i<actuator_vector.size(); i++) {
+	    actuator_vector[i].update();
 	}
     }
     ~KondoICSHW () {
-	listMotors();
+	//listMotors();
 	ics_close (&ics);
     }
-
     ros::Time getTime() const {return ros::Time::now();}
     ros::Duration getPeriod() const {return ros::Duration(0.01);}
 
+#if 0
     void listMotors(void) {
 	for (int i=0; i<MAX_MOTOR_NUM; i++) {
 	    int ret = ics_pos(&ics, i, 0);
@@ -140,20 +189,16 @@ class KondoICSHW : public hardware_interface::RobotHW
 	    }
 	}
     }
+#endif
 };
-
-bool KondoICSHW::motor_power = false;
-
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "kondo_ics_driver");
     ros::NodeHandle nh;
 
-    KondoICSHW robot;
+    KondoICSHW robot(nh, argc-1, &argv[1]);
     controller_manager::ControllerManager cm(&robot, nh);
-
-    ros::ServiceServer service = nh.advertiseService("/set_power", robot.set_power);
 
     ros::Rate rate(1.0 / robot.getPeriod().toSec());
     ros::AsyncSpinner spinner(1);
